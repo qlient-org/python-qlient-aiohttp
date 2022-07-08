a2401cfb2e816143e2b436d996b4fdb59e58dfac
Daniel Seifert <info@danielseifert.ch>
2022-07-08 12:50:58 +0200

    fix(subscription): initial start request was missing

---
+++ src/qlient/aiohttp/__init__.py
@@ -0,0 +1,2 @@
+from qlient.aiohttp.backends import AIOHTTPBackend
+from qlient.aiohttp.clients import AIOHTTPClient
--- src/qlient/aiohttp/backends.py
+++ src/qlient/aiohttp/backends.py
@@ -1,17 +1,45 @@
 import logging
+import uuid
 from contextlib import asynccontextmanager
 from typing import Optional, Dict, Any, AsyncGenerator, List
 
 import aiohttp
 import qlient.core.__meta__
-from qlient.core import AsyncBackend, GraphQLRequest, GraphQLResponse
+from qlient.core import AsyncBackend, GraphQLRequest, GraphQLResponse, GraphQLSubscriptionRequest
 
 from qlient.aiohttp.exceptions import ConnectionRejected
 
 logger = logging.getLogger(qlient.core.__meta__.__title__)
 
+# Protocols
+GRAPHQL_WS_PROTOCOL = "graphql-ws"
+GRAPHQL_TRANSPORT_WS_PROTOCOL = "graphql-transport-ws"
+
+# GQL Control Strings
+CONNECTION_INIT = "connection_init"
+CONNECTION_ACKNOWLEDGED = "connection_ack"
+CONNECTION_ERROR = "connection_error"
+CONNECTION_KEEP_ALIVE = "ka"
+START = "start"
+STOP = "stop"
+CONNECTION_TERMINATE = "connection_terminate"
+DATA = "data"
+ERROR = "error"
+COMPLETE = "complete"
+
+SUBSCRIPTION_ID_TO_WS = {}
+
 
 class AIOHTTPBackend(AsyncBackend):
+
+    @classmethod
+    def generate_subscription_id(cls) -> str:
+        """Class method to generate unique subscription ids
+
+        Returns:
+            A unique subscription id
+        """
+        return f"qlient:{cls.__name__}:{uuid.uuid4()}".replace("-", "")
 
     @staticmethod
     def adapt_to_websocket_endpoint(endpoint: str) -> str:
@@ -46,14 +74,6 @@ class AIOHTTPBackend(AsyncBackend):
             "variables": request.variables,
         }
 
-    @staticmethod
-    async def make_generator(socket: aiohttp.ClientWebSocketResponse) -> AsyncGenerator:
-        msg: aiohttp.WSMessage
-        async for msg in socket:
-            if msg.type != aiohttp.WSMsgType.TEXT:
-                continue
-            yield msg.json()
-
     def __init__(
             self,
             endpoint: str,
@@ -65,7 +85,7 @@ class AIOHTTPBackend(AsyncBackend):
             ws_endpoint = AIOHTTPBackend.adapt_to_websocket_endpoint(endpoint)
 
         if not subscription_protocols:
-            subscription_protocols = ["graphql-ws", "graphql-transport-ws"]
+            subscription_protocols = [GRAPHQL_WS_PROTOCOL, GRAPHQL_TRANSPORT_WS_PROTOCOL]
 
         self.endpoint: str = endpoint
         self.ws_endpoint: str = ws_endpoint
@@ -83,6 +103,14 @@ class AIOHTTPBackend(AsyncBackend):
             yield session
 
     async def execute_query(self, request: GraphQLRequest) -> GraphQLResponse:
+        """
+
+        Args:
+            request:
+
+        Returns:
+
+        """
         payload = self.make_payload(request)
         async with self.session as session:
             async with session.post(self.endpoint, json=payload) as response:
@@ -90,21 +118,65 @@ class AIOHTTPBackend(AsyncBackend):
                 return GraphQLResponse(request, response_body)
 
     async def execute_mutation(self, request: GraphQLRequest) -> GraphQLResponse:
+        """
+
+        Args:
+            request:
+
+        Returns:
+
+        """
         return await self.execute_query(request)
 
-    async def execute_subscription(self, request: GraphQLRequest) -> GraphQLResponse:
+    async def execute_subscription(self, request: GraphQLSubscriptionRequest) -> GraphQLResponse:
+        """
+
+        Args:
+            request:
+
+        Returns:
+
+        """
         payload = self.make_payload(request)
         async with self.session as session:
-            async with session.ws_connect(self.endpoint, protocols=self.subscription_protocols) as ws:
-                # initiate connection
-                await ws.send_json({"type": "connection_init", "payload": {}})
+            request.subscription_id = request.subscription_id or self.generate_subscription_id()
+            ws = await session.ws_connect(
+                self.endpoint,
+                protocols=self.subscription_protocols,
+                autoclose=False
+            )
+            # initiate connection
+            await ws.send_json({"type": CONNECTION_INIT, "payload": request.options})
 
-                initial_response = await ws.receive_json()
-                if initial_response.get("type") != "connection_ack":
-                    logger.critical(f"The server did not acknowledged the connection.")
-                    raise ConnectionRejected("The server did not acknowledge the connection.")
+            initial_response = await ws.receive_json()
+            if initial_response.get("type") != CONNECTION_ACKNOWLEDGED:
+                logger.critical(f"The server did not acknowledged the connection.")
+                raise ConnectionRejected("The server did not acknowledge the connection.")
 
-                # connection acknowledged, send request
-                await ws.send_json(payload)
+            # connection acknowledged, send request
+            await ws.send_json({"type": START, "id": request.subscription_id, "payload": payload})
 
-                return GraphQLResponse(request, self.make_generator(ws))
+            SUBSCRIPTION_ID_TO_WS[request.subscription_id] = ws
+
+            async def _make_generator() -> AsyncGenerator:
+                msg: aiohttp.WSMessage
+                async for msg in ws:
+                    if msg.type == aiohttp.WSMsgType.ERROR:
+                        # break the iterator
+                        break
+                    if msg.type != aiohttp.WSMsgType.TEXT:
+                        raise TypeError(f"Expected {aiohttp.WSMsgType.TEXT}; Got {msg.type}")
+
+                    data = msg.json()
+                    data_type = data["type"]
+
+                    if data_type in (CONNECTION_TERMINATE, CONNECTION_ERROR, COMPLETE):
+                        # break the iterator
+                        break
+
+                    if data_type == CONNECTION_KEEP_ALIVE:
+                        continue
+
+                    yield GraphQLResponse(request, data["payload"])
+
+            return GraphQLResponse(request, _make_generator())
